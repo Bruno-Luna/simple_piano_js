1f815cb8841d89f97680c255d11e40f0f3329ece
Bruno Luna <luna.brsilva@gmail.com>
2022-11-05 18:03:11 -0300

    inserção da classe dinâmica

--- assets/script/app.js
+++ assets/script/app.js
@@ -11,34 +11,41 @@ const nota = new Audio;
 notaDo.onclick = () => {
     nota.src = 'assets/notes/C.mp3'
     nota.play()
+    notaDo.classList.add('notaClicada')
 }
 
 notaRe.onclick = () => {
     nota.src = 'assets/notes/D.mp3'
     nota.play()
+    notaRe.classList.add('notaClicada')
 }
 
 notaMi.onclick = () => {
     nota.src = 'assets/notes/E.mp3'
     nota.play()
+    notaMi.classList.add('notaClicada')
 }
 
 notaFa.onclick = () => {
     nota.src = 'assets/notes/F.mp3'
     nota.play()
+    notaFa.classList.add('notaClicada')
 }
 
 notaSol.onclick = () => {
     nota.src = 'assets/notes/G.mp3'
     nota.play()
+    notaSol.classList.add('notaClicada')
 }
 
 notaLa.onclick = () => {
     nota.src = 'assets/notes/A.mp3'
     nota.play()
+    notaLa.classList.add('notaClicada')
 }
 
 notaSi.onclick = () => {
     nota.src = 'assets/notes/B.mp3'
     nota.play()
+    notaSi.classList.add('notaClicada')
 }
